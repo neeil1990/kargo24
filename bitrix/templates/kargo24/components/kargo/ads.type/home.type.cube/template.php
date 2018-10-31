b4f2f5aef5c8be0137b852d190049d86e6f90a78
neeil1990 <neeil@mail.ru>
2018-10-31 12:36:30 +0300

    no message

--- bitrix/templates/kargo24/components/kargo/ads.type/home.type.cube/template.php
+++ bitrix/templates/kargo24/components/kargo/ads.type/home.type.cube/template.php
@@ -2,6 +2,22 @@
 
 <? if(count($arResult['ITEMS']) > 0): ?>
     <div class="<?=$arParams['CLASS_BODY']?> wrapper-unified-column <?=$arParams['CLASS_TITLE']?>">
+        <? foreach ($arResult['ITEMS'] as &$arSection): ?>
+            <div class="unified-column">
+                <div class="unified-unit-service">
+                    <a href="/<?=$arResult['IBLOCK_FOLDER']?>/filter/type-is-<?=mb_strtolower($arSection['UF_XML_ID'])?>/apply/">
+                        <div class="item-img">
+                            <img src="<?=$arSection['PICTURE'] ?>" alt="<?=$arSection['UF_NAME']?>">
+                        </div>
+                        <div class="item-desc">
+                            <h3 class="title">
+                                <?=$arSection['UF_NAME']?>
+                            </h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        <? endforeach; ?>
 
         <? if(count($arParams['ADDITIONAL_BLOCK']) > 0):?>
 
@@ -23,22 +39,6 @@
             <? endforeach; ?>
 
         <? endif; ?>
-
-        <? foreach ($arResult['ITEMS'] as &$arSection): ?>
-            <div class="unified-column">
-                <div class="unified-unit-service">
-                    <a href="/<?=$arResult['IBLOCK_FOLDER']?>/filter/type-is-<?=mb_strtolower($arSection['UF_XML_ID'])?>/apply/">
-                        <div class="item-img">
-                            <img src="<?=$arSection['PICTURE'] ?>" alt="<?=$arSection['UF_NAME']?>">
-                        </div>
-                        <div class="item-desc">
-                            <h3 class="title">
-                                <?=$arSection['UF_NAME']?>
-                            </h3>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        <? endforeach; ?>
+        
     </div>
 <? endif; ?>
