ae80b2ef62c68a49e5fe5a81521f09c900a7ef8f
neeil1990 <neeil@mail.ru>
2020-04-30 16:51:14 +0300

    no message

--- bitrix/templates/kargo24/components/bitrix/breadcrumb/breadcrumb/template.php
+++ bitrix/templates/kargo24/components/bitrix/breadcrumb/breadcrumb/template.php
@@ -24,7 +24,8 @@ for($index = 0; $index < $itemSize; $index++)
 	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
 	{
         if(preg_match("/filter\\/(.*)\\/apply\\//", $APPLICATION->GetCurPage(),$filter_url)){
-            $arResult[$index]["LINK"] .= ($filter_url[0]) ? $filter_url[0] : null;
+            if($index)
+                $arResult[$index]["LINK"] .= ($filter_url[0]) ? $filter_url[0] : null;
         }
 		$strReturn .= '<li><a href="'.$arResult[$index]["LINK"].'">'.strtolower($title).' </a></li>';
 	}
