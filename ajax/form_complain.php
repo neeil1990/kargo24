17d434095765949f4946e2d0178f24f332798710
neeil1990 <neeil@mail.ru>
2020-07-06 16:26:44 +0300

    no message

--- ajax/form_complain.php
+++ ajax/form_complain.php
@@ -11,13 +11,29 @@ $arEventFields = array(
     "PHONE" => $_REQUEST['phone'],
     "EMAIL" => $_REQUEST['email'],
     "DESC" => $_REQUEST['desc'],
-    "LINK" => "/bitrix/admin/iblock_element_edit.php?IBLOCK_ID=$_REQUEST[IBLOCK_ID]&type=content&ID=$_REQUEST[ID]&lang=ru",
+    "LINK" => ($_REQUEST['ID']) ? "/bitrix/admin/iblock_element_edit.php?IBLOCK_ID=$_REQUEST[IBLOCK_ID]&type=content&ID=$_REQUEST[ID]&lang=ru" : null,
 );
-if(CEvent::Send("COMPLAIN_FORM", SITE_ID, $arEventFields)){
-    $arResult['STATUS'] = true;
-}else{
+
+if($recaptcha = $_REQUEST['g-recaptcha-response']){
+
+    $google_url = "https://www.google.com/recaptcha/api/siteverify";
+    $secret = '6LfwPakZAAAAANjtFRSGMa1fRL6Y16EkYlQrLR9A';
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $url = $google_url."?secret=".$secret."&response=".$recaptcha."&remoteip=".$ip;
+    $res = file_get_contents($url, true);
+    $res = json_decode($res, true);
+
+    if(!$res['success']){
+        $arResult['STATUS'] = false;
+    }else{
+        if(CEvent::Send("COMPLAIN_FORM", SITE_ID, $arEventFields)){
+            $arResult['STATUS'] = true;
+        }else{
+            $arResult['STATUS'] = false;
+        }
+    }
+}else
     $arResult['STATUS'] = false;
-}
 
 return print json_encode($arResult);
 ?>
